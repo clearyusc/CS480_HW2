e5a5cf1945efa7a15568e06e067d6d16abc5d355
Qing Chen <qingchen@itswphimac045.ics.usc.edu>
2012-09-20 17:18:01 -0700

    Initial Commit

---
+++ assign1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  assign1
+//
+//  Created by  on 9/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
